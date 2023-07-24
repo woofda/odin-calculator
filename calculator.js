31ebabf214f93d08c32ced99f23795c44dd9fa53
woofda <130379195+woofda@users.noreply.github.com>
2023-07-24 17:11:48 -0500

    Add operate function

--- calculator.js
+++ calculator.js
@@ -5,3 +5,20 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+
+const operate = (a, b, operand) => {
+    switch(operand) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return "Error";
+    }
+}
+
+let firstNumber, secondNumber, operand;
