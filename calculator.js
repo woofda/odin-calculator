e094b968574953273e094150220d7ef20a6a9421
woofda <130379195+woofda@users.noreply.github.com>
2023-07-24 22:57:20 -0500

    Add equals button functionality and fix repeat operators

--- calculator.js
+++ calculator.js
@@ -2,7 +2,10 @@
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const divide = (a, b) => {
+    if (b === 0) return NaN;
+    else return a / b;
+}
 
 const operate = (a, b, operand) => {
     switch(operand) {
@@ -23,11 +26,17 @@ const operate = (a, b, operand) => {
 let firstNumber, secondNumber, operator;
 let displayValue = '';
 let resetDisplay = false;
+let numberUpdated = true;
 
 const display = document.querySelector('.display');
 
 const updateDisplay = () => {
-    if(!(displayValue === undefined || displayValue === '')) {
+    if(isNaN(displayValue)) {
+        firstNumber = undefined;
+        secondNumber = undefined;
+        displayValue = '0';
+        display.innerText = "Hey, you can't do that!";
+    } else if(!(displayValue === undefined || displayValue === '')) {
         display.innerText = displayValue;
     }
 }
@@ -36,6 +45,8 @@ const clear = () => {
     displayValue = '0';
     firstNumber = undefined;
     secondNumber = undefined;
+    operator = undefined;
+    numberUpdated = true;
     updateDisplay();
 }
 
@@ -45,25 +56,40 @@ const inputNumber = (button) => {
         displayValue = button.innerText;
         updateDisplay();
         resetDisplay = false;
+        numberUpdated = true;
     } else {
         displayValue += button.innerText;
         updateDisplay();
+        numberUpdated = true;
     }
 }
 
 const inputOperator = (button) => {
     if(firstNumber === undefined) {
         firstNumber = parseFloat(displayValue);
-    } else {
+    } else if(numberUpdated) {
         secondNumber = parseFloat(displayValue);
         displayValue = operate(firstNumber, secondNumber, operator);
         updateDisplay();
         firstNumber = parseFloat(displayValue);
     }
 
+    numberUpdated = false;
     operator = button.innerText;
     resetDisplay = true;
     // Add some button effect here
+}
+
+const inputEquals = () => {
+    if(operator !== undefined) {
+        secondNumber = parseFloat(displayValue);
+        displayValue = operate(firstNumber, secondNumber, operator);
+        updateDisplay();
+        firstNumber = undefined;
+        secondNumber = undefined;
+        operator = undefined;
+        resetDisplay = true;
+    }
 }
 
 // Number button listeners
@@ -74,6 +100,7 @@ numberButtons.forEach((button) => {
     });
 });
 
+// Operator button listeners
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -85,3 +112,8 @@ operatorButtons.forEach((button) => {
 document.querySelector('.clear').addEventListener('click', () => {
     clear();
 });
+
+// Equals button listener
+document.querySelector('.equals').addEventListener('click', () => {
+    inputEquals();
+})
