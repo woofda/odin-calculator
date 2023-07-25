77ad2533b6aa3c82fde003fa2f7e93be55f6f712
woofda <130379195+woofda@users.noreply.github.com>
2023-07-24 20:36:24 -0500

    Add functionality to numbers and operators

--- calculator.js
+++ calculator.js
@@ -1,9 +1,7 @@
+// Arithmatic functions
 const add = (a, b) => a + b;
-
 const subtract = (a, b) => a - b;
-
 const multiply = (a, b) => a * b;
-
 const divide = (a, b) => a / b;
 
 const operate = (a, b, operand) => {
@@ -12,13 +10,78 @@ const operate = (a, b, operand) => {
             return add(a, b);
         case '-':
             return subtract(a, b);
-        case '*':
+        case 'x':
             return multiply(a, b);
-        case '/':
+        case '÷':
             return divide(a, b);
         default:
             return "Error";
     }
 }
 
-let firstNumber, secondNumber, operand;
+// Display and number variables
+let firstNumber, secondNumber, operator;
+let displayValue = '';
+let resetDisplay = false;
+
+const display = document.querySelector('.display');
+
+const updateDisplay = () => {
+    if(!(displayValue === undefined || displayValue === '')) {
+        display.innerText = displayValue;
+    }
+}
+
+const clear = () => {
+    displayValue = '0';
+    firstNumber = undefined;
+    secondNumber = undefined;
+    updateDisplay();
+}
+
+// Event called to add numbers to display
+const inputNumber = (button) => {
+    if(displayValue === '' || displayValue === '0' || resetDisplay) {
+        displayValue = button.innerText;
+        updateDisplay();
+        resetDisplay = false;
+    } else {
+        displayValue += button.innerText;
+        updateDisplay();
+    }
+}
+
+const inputOperator = (button) => {
+    if(firstNumber === undefined) {
+        firstNumber = parseFloat(displayValue);
+    } else {
+        secondNumber = parseFloat(displayValue);
+        displayValue = operate(firstNumber, secondNumber, operator);
+        updateDisplay();
+        firstNumber = parseFloat(displayValue);
+    }
+
+    operator = button.innerText;
+    resetDisplay = true;
+    // Add some button effect here
+}
+
+// Number button listeners
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        inputNumber(button);
+    });
+});
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        inputOperator(button);
+    })
+})
+
+// Clear button listener
+document.querySelector('.clear').addEventListener('click', () => {
+    clear();
+});
